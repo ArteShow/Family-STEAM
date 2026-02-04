217360eae1dcf820ff647bd85eb8f028ce2bc37f
ArteShow <sokartemax@gmail.com>
2026-02-04 19:45:00 +0100

    add postgres db and migrations v1

--- migrations/0001_init.up.sql
+++ migrations/0001_init.up.sql
@@ -1,3 +1,9 @@
-CREATE TABLE IF NOT EXISTS calender (
-    
+CREATE TABLE IF NOT EXISTS calendar (
+    id TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    event_type TEXT NOT NULL,
+    starts_at TIMESTAMPTZ NOT NULL,
+    ends_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
