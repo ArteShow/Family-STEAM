7701220c5b374f3d2a7e695756cb0d806721d081
ArteShow <sokartemax@gmail.com>
2026-02-05 17:23:22 +0100

    removed hashing

--- services/auth-service/go.mod
+++ services/auth-service/go.mod
@@ -5,7 +5,6 @@ go 1.24.4
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/ilyakaznacheev/cleanenv v1.5.0
-	golang.org/x/crypto v0.44.0
 	google.golang.org/grpc v1.78.0
 	google.golang.org/protobuf v1.36.11
 )
--- services/auth-service/internal/core/login.go
+++ services/auth-service/internal/core/login.go
@@ -6,7 +6,6 @@ import (
 	"github.com/ArteShow/Family-STEAM/services/auth-service/internal/client"
 	jwtutile "github.com/ArteShow/Family-STEAM/services/auth-service/internal/jwt"
 	"github.com/ArteShow/Family-STEAM/services/auth-service/internal/proto"
-	"github.com/ArteShow/Family-STEAM/services/auth-service/pkg/hashing"
 )
 
 func Login(username, password string) (string, error) {
@@ -16,14 +15,9 @@ func Login(username, password string) (string, error) {
 	}
 	defer client.Close()
 
-	hash, err := hashing.HashPassword(password)
-	if err != nil {
-		return "", err
-	}
-
 	res, err := client.GetUserPassword(&proto.GetUsersIDRequest{
 		Username: username,
-		Password: string(hash),
+		Password: password,
 	})
 
 	if err != nil {
