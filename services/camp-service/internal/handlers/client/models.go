cc955f4b37f9fec6b9785a990c8bfdf68572bc39
ArteShow <sokartemax@gmail.com>
2026-02-06 21:33:58 +0100

    add create handler for client

--- services/camp-service/internal/handlers/client/models.go
+++ services/camp-service/internal/handlers/client/models.go
@@ -1,1 +1,48 @@
 package handlers
+
+type CreateClientRequest struct {
+	Name       string `json:"name"`
+	FamilyName string `json:"family_name"`
+	BirthDate  string `json:"birth_date"`
+	Email      string `json:"email"`
+	Number     string `json:"number"`
+	Type       string `json:"type"`
+}
+
+type CreateClientResponse struct {
+	ClientID string `json:"Client_id"`
+}
+
+type DeleteClientRequest struct {
+	ClientID string `json:"Client_id"`
+}
+
+type GetAllClientResponse struct {
+	Clients []struct {
+		ID         string `json:"client_id"`
+		Name       string `json:"name"`
+		FamilyName string `json:"family_name"`
+		BirthDate  string `json:"birth_date"`
+		Email      string `json:"email"`
+		Number     string `json:"number"`
+		Type       string `json:"type"`
+		CreatedAt  string `json:"created_at"`
+	} `json:"clients"`
+}
+
+type GetCampByIdRequest struct {
+	ClientID string `json:"client_id"`
+}
+
+type GetCampByIdResponse struct {
+	Client struct {
+		ID         string `json:"client_id"`
+		Name       string `json:"name"`
+		FamilyName string `json:"family_name"`
+		BirthDate  string `json:"birth_date"`
+		Email      string `json:"email"`
+		Number     string `json:"number"`
+		Type       string `json:"type"`
+		CreatedAt  string `json:"created_at"`
+	} `json:"client"`
+}
