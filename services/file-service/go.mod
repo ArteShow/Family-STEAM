2587ff73d68125a0bc9cdc0b087e57b5ea5489fe
ArteShow <sokartemax@gmail.com>
2026-02-07 12:37:15 +0100

    add docker package

--- services/file-service/go.mod
+++ services/file-service/go.mod
@@ -3,6 +3,8 @@ module github.com/ArteShow/Family-STEAM/services/file-service
 go 1.24.4
 
 require (
+	github.com/docker/docker v28.5.2+incompatible
+	github.com/google/uuid v1.6.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/lib/pq v1.11.1
 	google.golang.org/grpc v1.78.0
@@ -11,11 +13,39 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/containerd/errdefs v1.0.0 // indirect
+	github.com/containerd/errdefs/pkg v0.3.0 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-connections v0.6.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/sys v0.38.0 // indirect
-	golang.org/x/text v0.31.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/term v0.5.2 // indirect
+	github.com/morikuni/aec v1.1.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0 // indirect
+	go.opentelemetry.io/otel v1.40.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.40.0 // indirect
+	go.opentelemetry.io/otel/metric v1.40.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.40.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.40.0 // indirect
+	go.opentelemetry.io/otel/trace v1.40.0 // indirect
+	golang.org/x/net v0.49.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
+	golang.org/x/time v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260128011058-8636f8732409 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gotest.tools/v3 v3.5.2 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- services/file-service/internal/docker/volumes.go
+++ services/file-service/internal/docker/volumes.go
@@ -1,1 +1,142 @@
 package docker
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"io"
+	"path/filepath"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
+	"github.com/google/uuid"
+)
+
+const volumeName = "family-steam"
+
+func getClient() (*client.Client, error) {
+	return client.NewClientWithOpts(client.FromEnv)
+}
+
+func UploadFile(data []byte, fileName string) (string, error) {
+	cli, err := getClient()
+	if err != nil {
+		return "", err
+	}
+
+	id := uuid.New().String()
+
+	ctx := context.Background()
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine",
+		Cmd:   []string{"sleep", "5"},
+	}, &container.HostConfig{
+		Binds: []string{volumeName + ":/data"},
+	}, &network.NetworkingConfig{}, nil, "")
+	if err != nil {
+		return "", err
+	}
+
+	containerID := resp.ID
+
+	defer cli.ContainerRemove(ctx, containerID, container.RemoveOptions{Force: true})
+
+	err = cli.ContainerStart(ctx, containerID, container.StartOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+	tw := tar.NewWriter(&buf)
+
+	tw.WriteHeader(&tar.Header{
+		Name: filepath.Join(id, fileName),
+		Mode: 0600,
+		Size: int64(len(data)),
+	})
+
+	tw.Write(data)
+	tw.Close()
+
+	err = cli.CopyToContainer(ctx, containerID, "/data", &buf, container.CopyToContainerOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
+}
+
+func DownloadFile(id string, fileName string) ([]byte, error) {
+	cli, err := getClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine",
+		Cmd:   []string{"sleep", "5"},
+	}, &container.HostConfig{
+		Binds: []string{volumeName + ":/data"},
+	}, &network.NetworkingConfig{}, nil, "")
+	if err != nil {
+		return nil, err
+	}
+
+	containerID := resp.ID
+
+	defer cli.ContainerRemove(ctx, containerID, container.RemoveOptions{Force: true})
+
+	err = cli.ContainerStart(ctx, containerID, container.StartOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	reader, _, err := cli.CopyFromContainer(ctx, containerID, filepath.Join("/data", id, fileName))
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	tr := tar.NewReader(reader)
+
+	_, err = tr.Next()
+	if err != nil {
+		return nil, err
+	}
+
+	return io.ReadAll(tr)
+}
+
+func DeleteFile(id string) error {
+	cli, err := getClient()
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine",
+		Cmd:   []string{"rm", "-rf", "/data/" + id},
+	}, &container.HostConfig{
+		Binds: []string{volumeName + ":/data"},
+	}, &network.NetworkingConfig{}, nil, "")
+	if err != nil {
+		return err
+	}
+
+	containerID := resp.ID
+
+	defer cli.ContainerRemove(ctx, containerID, container.RemoveOptions{Force: true})
+
+	err = cli.ContainerStart(ctx, containerID, container.StartOptions{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
