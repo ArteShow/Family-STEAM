82d2ab2a3a710efc9ec2ab902ca3b1098fd4f507
ArteShow <sokartemax@gmail.com>
2026-02-07 14:24:15 +0100

    add main package and port to config

--- services/file-service/cmd/file-service/main.go
+++ services/file-service/cmd/file-service/main.go
@@ -1,1 +1,83 @@
 package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/ArteShow/Family-STEAM/services/file-service/internal/config"
+	"github.com/ArteShow/Family-STEAM/services/file-service/internal/handlers"
+	"github.com/ArteShow/Family-STEAM/services/file-service/internal/middleware"
+)
+
+const (
+	readTimeout  = 10 * time.Second
+	writeTimeout = 10 * time.Second
+	idleTimeou   = 60 * time.Second
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cfg.Port != "" && cfg.Port[0] != ':' {
+		cfg.Port = ":" + cfg.Port
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/camp-service/health", func(w http.ResponseWriter, _ *http.Request) {
+		_, err = w.Write([]byte("ok"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+	mux.Handle("/file-service/upload", middleware.AdminCheckMiddleware(handlers.UploadFile))
+	mux.Handle("/file-service/download", middleware.AdminCheckMiddleware(handlers.DownloadFile))
+	mux.Handle("/file-service/get_all", middleware.AdminCheckMiddleware(handlers.GetAllFiles))
+	mux.Handle("/file-service/get_by_id", middleware.AdminCheckMiddleware(handlers.GetFileByID))
+
+	srv := &http.Server{
+		Addr:         cfg.Port,
+		Handler:      mux,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		IdleTimeout:  idleTimeou,
+	}
+
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
+	go func() {
+		log.Println("server running on :8004")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	log.Println("graceful shutdown started")
+
+	shutdownCtx, cancel := context.WithTimeout(
+		context.Background(),
+		10*time.Second,
+	)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Printf("shutdown failed: %v", err)
+	}
+
+	log.Println("shutdown complete")
+}
