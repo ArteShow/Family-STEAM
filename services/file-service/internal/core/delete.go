127eaf6a674cb619043a5bf6c9a4581fda0a5c46
ArteShow <sokartemax@gmail.com>
2026-02-07 12:48:00 +0100

    add delete function

--- services/file-service/internal/core/delete.go
+++ services/file-service/internal/core/delete.go
@@ -1,1 +1,20 @@
 package core
+
+import (
+	"context"
+
+	"github.com/ArteShow/Family-STEAM/services/file-service/internal/docker"
+	"github.com/ArteShow/Family-STEAM/services/file-service/internal/repository"
+)
+
+func DeleteFile(id string) error {
+	if err := docker.DeleteFile(id); err != nil {
+		return err
+	}
+
+	if err := repository.DeleteFile(context.Background(), id); err != nil {
+		return err
+	}
+
+	return nil
+}
