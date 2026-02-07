c5fa4b029182342284f7dfb7ae1aded6eab3a133
ArteShow <sokartemax@gmail.com>
2026-02-07 19:08:41 +0100

    add all handlers

--- services/news-service/internal/handlers/models.go
+++ services/news-service/internal/handlers/models.go
@@ -1,1 +1,29 @@
 package handlers
+
+import "github.com/ArteShow/Family-STEAM/services/news-service/internal/repository"
+
+type CreateNewsRequest struct {
+	Title       string `json:"title"`
+	ImageID     string `json:"image_id"`
+	Description string `json:"description"`
+}
+
+type CreateNewsResponse struct {
+	NewsID string `json:"news_id"`
+}
+
+type DeleteNewsRequest struct {
+	NewsID string `json:"news_id"`
+}
+
+type GetAllNewsResponse struct {
+	News []repository.News `json:"news"`
+}
+
+type GetNewsByIdRequest struct {
+	NewsID string `json:"news_id"`
+}
+
+type GetNewsByIdResponse struct{
+	News repository.News `json:"news"`
+}
