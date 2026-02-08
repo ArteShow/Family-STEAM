405d0c903b818065050aa1299332fabd6ff26d07
ArteShow <sokartemax@gmail.com>
2026-02-08 10:06:56 +0100

    add all handlers

--- services/feedback-service/internal/handlers/get_all.go
+++ services/feedback-service/internal/handlers/get_all.go
@@ -1,1 +1,27 @@
 package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/ArteShow/Family-STEAM/services/feedback-service/internal/repository"
+)
+
+func GetAllHandler(w http.ResponseWriter, r *http.Request) {
+	feedbacks, err := repository.GetAll(context.Background())
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res := GetAllResponse{Feedbacks: feedbacks}
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err = json.NewEncoder(w).Encode(res); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
--- services/feedback-service/internal/handlers/get_by_id.go
+++ services/feedback-service/internal/handlers/get_by_id.go
@@ -1,1 +1,44 @@
 package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/ArteShow/Family-STEAM/services/feedback-service/internal/repository"
+)
+
+func GetByIDHandler(w http.ResponseWriter, r *http.Request) {
+	var req GetByIDRequest
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	if err = json.Unmarshal(body, &req); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	feedback, err := repository.GetByID(
+		context.Background(),
+		req.FeedbackID,
+	)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res := GetByIDResponse{Feedback: feedback}
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err = json.NewEncoder(w).Encode(res); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
