9f37e3342d43475960d2440a0c424c6d9cd582a0
ArteShow <sokartemax@gmail.com>
2026-02-12 14:00:44 +0100

    add dynamix slider

---
+++ frontend/public/assets/js/image_slider.js
@@ -0,0 +1,15 @@
+const images = [
+    "assets/images/slider1.jpg",
+    "assets/images/slider2.jpg",
+];
+
+let index = 0;
+const slider = document.querySelector(".background_slider");
+
+function changeBackground() {
+    slider.style.backgroundImage = `url(${images[index]})`;
+    index = (index + 1) % images.length;
+}
+
+changeBackground(); 
+setInterval(changeBackground, 5000); 
