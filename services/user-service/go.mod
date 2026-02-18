c35ad668d0473de7c3f5e3b6bb169c4529d2c3c5
ArteShow <sokartemax@gmail.com>
2026-02-18 21:29:04 +0100

    add repository package

--- services/user-service/go.mod
+++ services/user-service/go.mod
@@ -11,10 +11,12 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/net v0.48.0 // indirect
-	golang.org/x/sys v0.39.0 // indirect
-	golang.org/x/text v0.32.0 // indirect
+	golang.org/x/crypto v0.48.0 // indirect
+	golang.org/x/net v0.49.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
--- services/user-service/internal/repository/user_repo.go
+++ services/user-service/internal/repository/user_repo.go
@@ -1,1 +1,56 @@
 package repository
+
+import (
+	"time"
+
+	"github.com/ArteShow/Family-STEAM/services/user-service/internal/database"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func CreateUser(username string, password string) error {
+	db, err := database.Connect()
+	if err != nil {
+		return err
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	id := uuid.New().String()
+
+	_, err = db.Exec(
+		`INSERT INTO users (id, username, password, created_at) 
+		 VALUES ($1, $2, $3, $4)`,
+		id,
+		username,
+		string(hash),
+		time.Now(),
+	)
+
+	return err
+}
+
+func CheckUserLogin(username string, password string) bool {
+	db, err := database.Connect()
+	if err != nil {
+		return false
+	}
+
+	var hash string
+
+	err = db.QueryRow(
+		`SELECT password FROM users WHERE username = $1`,
+		username,
+	).Scan(&hash)
+
+	if err != nil {
+		return false
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
