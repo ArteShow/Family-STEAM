1cc6428f79ba51f99f39a73fd240adb13637e8ad
ArteShow <sokartemax@gmail.com>
2026-02-19 14:36:07 +0100

    add CheckUsserId funtioon to repo

--- services/user-service/internal/repository/user_repo.go
+++ services/user-service/internal/repository/user_repo.go
@@ -54,3 +54,23 @@ func CheckUserLogin(username string, password string) bool {
 	err = bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
+
+func CheckUsersId(username string, id string) bool {
+	db, err := database.Connect()
+	if err != nil {
+		return false
+	}
+
+	var hash string
+
+	err = db.QueryRow(
+		`SELECT id FROM users WHERE username = $1`,
+		username,
+	).Scan(&hash)
+
+	if err != nil {
+		return false
+	}
+
+	return true
+}
