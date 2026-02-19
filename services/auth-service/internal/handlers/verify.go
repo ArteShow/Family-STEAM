f8a57a12bbd192f0377eac26703684b23c161089
ArteShow <sokartemax@gmail.com>
2026-02-19 15:44:10 +0100

    add verify handler

--- services/auth-service/internal/handlers/verify.go
+++ services/auth-service/internal/handlers/verify.go
@@ -1,1 +1,41 @@
 package handlers
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/ArteShow/Family-STEAM/services/auth-service/internal/client"
+	"github.com/ArteShow/Family-STEAM/services/auth-service/internal/proto"
+)
+
+func VerifyHandler(w http.ResponseWriter, r *http.Request) {
+	bytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var req CheckUserIdRequest
+	err = json.Unmarshal(bytes, &req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	userClient, err := client.NewUserClient()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer userClient.Close()
+	GrpcRes, err := userClient.CheckUserId(&proto.CheckUserIdRequest{
+		Id: req.Id,
+	})
+	if err != nil || !GrpcRes.Success{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
