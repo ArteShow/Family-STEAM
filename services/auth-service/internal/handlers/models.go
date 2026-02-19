255cdd8d551366342724db07e74517cb32093ca5
ArteShow <sokartemax@gmail.com>
2026-02-19 14:54:06 +0100

    add requests models

--- services/auth-service/internal/handlers/models.go
+++ services/auth-service/internal/handlers/models.go
@@ -1,1 +1,19 @@
 package handlers
+
+type RegisterRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type RegisterResponse struct {
+	ID string `json:"user_id"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
