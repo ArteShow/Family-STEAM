d3c669072e6ecabfc77bb978d4f588e17c83db29
ArteShow <sokartemax@gmail.com>
2026-02-20 15:10:24 +0100

    add real jwt key

--- services/auth-service/internal/config/config.go
+++ services/auth-service/internal/config/config.go
@@ -4,7 +4,8 @@ import "github.com/ilyakaznacheev/cleanenv"
 
 type Config struct {
 	Port      string `env:"AUTH_SERVICE_PORT" env-default:":8001"`
-	JWTSecret string `env:"JWT_SECRET" env-default:"dev-only"`
+	JWTSecret string `env:"JWT_SECRET" env-default:"9f4d7c2a6e3b1a8f5c9d0e4b7a2f6c1d8e3b5a9f0c7d2e6b4a1f8c3d5e9b7a2
+"`
 }
 
 func Read() (*Config, error) {
