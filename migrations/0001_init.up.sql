dd4d1135b851bf012da786eb1d984d11540feb36
ArteShow <sokartemax@gmail.com>
2026-02-21 13:30:53 +0100

    add new migration

--- migrations/0001_init.up.sql
+++ migrations/0001_init.up.sql
@@ -10,4 +10,17 @@ CREATE TABLE IF NOT EXISTS files (
     parent_id TEXT NOT NULL,
     file_name TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
-)
+);
+
+CREATE TABLE IF NOT EXISTS clients (
+    id TEXT PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    birthday DATE,
+    age INT,
+    camp TEXT,
+    event TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
