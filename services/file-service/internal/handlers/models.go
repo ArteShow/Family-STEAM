4b0dbf93f45a187bb8d27d539377ec51bbb692c4
ArteShow <sokartemax@gmail.com>
2026-02-21 13:02:32 +0100

    add upload handler

--- services/file-service/internal/handlers/models.go
+++ services/file-service/internal/handlers/models.go
@@ -1,1 +1,10 @@
 package handlers
+
+type UploadRequest struct {
+	ParentID string `json:"parent_id"`
+	FileName string `json:"file_name"`
+}
+
+type UploadResponse struct {
+	FileID string `json:"file_id"`
+}
--- services/file-service/internal/handlers/upload.go
+++ services/file-service/internal/handlers/upload.go
@@ -1,1 +1,53 @@
 package handlers
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/ArteShow/Family-STEAM/services/file-service/internal/core"
+)
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	var req UploadRequest
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	if err = json.Unmarshal(body, &req); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	r.ParseMultipartForm(10 << 20)
+
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "File error", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Read error", http.StatusInternalServerError)
+		return
+	}
+
+	id, err := core.UploadFile(req.FileName, req.ParentID, fileBytes)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res := UploadResponse{FileID: id}
+	w.WriteHeader(http.StatusOK)
+	if err = json.NewEncoder(w).Encode(res); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
