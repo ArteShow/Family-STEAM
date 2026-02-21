6c4f6e4041977609aba8a023f48cc5f83c028b6e
ArteShow <sokartemax@gmail.com>
2026-02-21 09:33:29 +0100

    add docker package

--- services/file-service/internal/docker/volumns.go
+++ services/file-service/internal/docker/volumns.go
@@ -1,1 +1,149 @@
 package docker
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"io"
+	"path"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/google/uuid"
+)
+
+const VolumeName = "family-steam"
+
+func UploadFile(fileBytes []byte, filename string) (string, error) {
+	ctx := context.Background()
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return "", err
+	}
+
+	id := uuid.New().String()
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine:3.19",
+		Cmd:   []string{"sleep", "60"},
+	}, &container.HostConfig{
+		Binds: []string{VolumeName + ":/data"},
+	}, nil, nil, "")
+	if err != nil {
+		return "", err
+	}
+
+	containerID := resp.ID
+
+	err = cli.ContainerStart(ctx, containerID, container.StartOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	buf := new(bytes.Buffer)
+	tw := tar.NewWriter(buf)
+
+	err = tw.WriteHeader(&tar.Header{
+		Name: path.Join(id, filename),
+		Mode: 0644,
+		Size: int64(len(fileBytes)),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	_, err = tw.Write(fileBytes)
+	if err != nil {
+		return "", err
+	}
+
+	tw.Close()
+
+	err = cli.CopyToContainer(ctx, containerID, "/data", buf, container.CopyToContainerOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	cli.ContainerRemove(ctx, containerID, container.RemoveOptions{Force: true})
+
+	return id, nil
+}
+
+func DownloadFile(id string, filename string) ([]byte, error) {
+	ctx := context.Background()
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine:3.19",
+		Cmd:   []string{"sleep", "60"},
+	}, &container.HostConfig{
+		Binds: []string{VolumeName + ":/data"},
+	}, nil, nil, "")
+	if err != nil {
+		return nil, err
+	}
+
+	containerID := resp.ID
+
+	err = cli.ContainerStart(ctx, containerID, container.StartOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	reader, _, err := cli.CopyFromContainer(ctx, containerID, path.Join("/data", id, filename))
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	tr := tar.NewReader(reader)
+
+	_, err = tr.Next()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := io.ReadAll(tr)
+	if err != nil {
+		return nil, err
+	}
+
+	cli.ContainerRemove(ctx, containerID, container.RemoveOptions{Force: true})
+
+	return data, nil
+}
+
+func RemoveFile(id string) error {
+	ctx := context.Background()
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return err
+	}
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine:3.19",
+		Cmd:   []string{"rm", "-rf", path.Join("/data", id)},
+	}, &container.HostConfig{
+		Binds: []string{VolumeName + ":/data"},
+	}, nil, nil, "")
+	if err != nil {
+		return err
+	}
+
+	containerID := resp.ID
+
+	err = cli.ContainerStart(ctx, containerID, container.StartOptions{})
+	if err != nil {
+		return err
+	}
+
+	cli.ContainerRemove(ctx, containerID, container.RemoveOptions{Force: true})
+
+	return nil
+}
